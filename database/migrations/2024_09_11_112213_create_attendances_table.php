c0f621004374c1485d72404806f243ec63bcb2db
Saabbass <178414@novacollege.nl>
2024-09-14 14:38:48 +0200

    feat: grammar correction all attendance

--- database/migrations/2024_09_11_112213_create_attendances_table.php
+++ database/migrations/2024_09_11_112213_create_attendances_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('attandances', function (Blueprint $table) {
+        Schema::create('attendances', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('attandances');
+        Schema::dropIfExists('attendances');
     }
 };
--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AttandanceSeeder extends Seeder
+class AttendanceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
