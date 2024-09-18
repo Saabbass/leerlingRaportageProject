3b60757f556d672df397e11a2ab0af4b7591cbcb
tim rogier <t.rogier@me.com>
2024-09-18 13:27:19 +0200

    Feat: functions for  subjects

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -12,5 +12,51 @@ class SubjectController extends Controller
         $subjects = Subject::all();
 
         return view('subject.index', ['subject' => $subjects]);
+    
+    }
+    public function create()
+    {
+        return view('subject.create');
+    }
+
+    public function edit($id)
+    {
+        $subject = Subject::findOrFail($id);
+
+        return view('subject.edit', ['subject' => $subject]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+        $subject = new Subject();
+        $subject->subject_name = $request->input('name');
+        $subject->description = $request->input('description');
+        $subject->save();
+
+        return redirect()->route('subject.index')->with('success', 'Subject created successfully.');
+    }
+    public function destroy($id)
+    {
+        $subject = Subject::findOrFail($id);
+        $subject->delete();
+
+        return redirect()->route('subject.index')->with('success', 'Subject deleted successfully.');
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $subject = Subject::findOrFail($id);
+        $subject->subject_name = $request->input('name');
+        $subject->description = $request->input('description');
+        $subject->save();
+
+        return redirect()->route('subject.index')->with('success', 'Subject updated successfully.');
     }
 }
