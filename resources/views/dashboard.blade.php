0151f79242aac78f7a6fa97b92adfe5f0c0f8d24
tim rogier <t.rogier@me.com>
2024-09-18 12:43:28 +0200

    Feat: dashboard_created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,22 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
+            {{-- <div class="mt-6">
+                <a href="{{ route('cijfers') }}" class="text-blue-500 hover:underline">Cijfers</a>
+                <a href="{{ route('klassen') }}" class="ml-4 text-blue-500 hover:underline">Klassen</a>
+            </div> --}}
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight">
+                        {{ __('Agenda') }}
+                    </h3>
+                    <ul class="mt-4">
+                        <li>{{ __('Event 1: Meeting with team at 10 AM') }}</li>
+                        <li>{{ __('Event 2: Project deadline at 3 PM') }}</li>
+                        <li>{{ __('Event 3: Call with client at 5 PM') }}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
