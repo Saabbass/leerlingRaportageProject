7d566ca4d5fcb47affa231b11bb594c1ad68c761
tim rogier <t.rogier@me.com>
2024-09-18 13:28:22 +0200

    Feat: subject.edit_created

---
+++ resources/views/subject/create.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create Subject') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form method="POST" action="{{ route('subject.store') }}">
+                        @csrf
+
+                        <div class="mb-4">
+                            <x-input-label for="name" :value="__('Subject Name')" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
+
+                        <div class="mb-4">
+                            <x-input-label for="description" :value="__('Description')" />
+                            <x-textarea-input id="description" class="block mt-1 w-full" name="description" :value="old('description')" />
+                            <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-primary-button class="ml-4">
+                                {{ __('Create Subject') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
