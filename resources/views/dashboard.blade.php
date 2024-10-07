4aa1cffd2d8e50427fd2519993deb2fb0e829e0a
tim rogier <t.rogier@me.com>
2024-10-07 10:22:50 +0200

    pushes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,9 @@
       {{ __('Dashboard') }}
     </h2>
   </x-slot>
-
+  @if (auth()->user()->role === 'teacher')
+    <a href="{{ route('users.index') }}" class="hover:underline rounded-xl hover:text-[#104E8B] dark:hover:text-[#FF6F61]">Users</a>
+  @endif
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-[#79b5ff] dark:bg-[#263238] overflow-hidden shadow-sm sm:rounded-lg">
