9d528511cd9b0cd1170afaaecd2b24c231a9cc9a
Saabbass <178414@novacollege.nl>
2024-10-25 10:59:26 +0200

    Feat: added test event for fullcalendar

---
+++ database/seeders/EventSubjectSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventSubjectSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    \App\Models\Event::create([
+      'subject_id' => '5',
+      'subject_name' => 'hellohello',
+      'start' => today(),
+      'end'=> today(),
+    ]);
+  }
+}
