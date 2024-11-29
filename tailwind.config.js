9ad5905b5f0d0e4fee1e7d055a046070ac01c98b
Nikita <nikitaeug@gmail.com>
2024-11-29 10:25:40 +0100

    feat: mooie notificatie

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,12 @@ export default {
             },
         },
         colors: {
+            // scrollbar colors
+            scrollbarLight: "rgba(74, 144, 226, 0.8)",
+            scrollbarLightTrack: "rgba(255, 255, 255, 0.2)",
+            scrollbarDark: "rgba(59, 89, 152, 0.8)",
+            scrollbarDarkTrack: "rgba(28, 28, 46, 0.2)",
+
             // Text colors
             primaryLightText: "#FFFFFF",
             secondaryLightText: "#333333",
